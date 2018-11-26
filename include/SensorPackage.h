ccedb30dc48e6d2cd130d0f4aca02d09dc2bbbe1
Jeremy <jeremymdunne@gmail.com>
2018-11-25 19:49:55 -0500

    Working on sensor package to get a simple altitude logger working

--- include/SensorPackage.h
+++ include/SensorPackage.h
@@ -1,7 +1,9 @@
 #ifndef _SENSOR_PACKAGE_H_
 #define _SENSOR_PACKAGE_H_
-#include <BMP280.h>
+
 #include "DataConfig.h"
+#include <UpdateScheduler.h>
+#include <BMP280.h>
 //flags for sensors, used in return flags and sensor type flags
 
 
@@ -11,6 +13,30 @@
 #define SENSOR_PACKAGE_GPS_UPDATE_FREQUENCY 1
 #define SENSOR_PACKAGE_WANTED_UPDATE_FREQUENCY 400
 
+#define GPS_SENSOR_INIT_FAILURE -70
+#define IMU_SENSOR_INIT_FAILURE -71
+#define PRESSURE_SENSOR_INIT_FAILURE -72
+
+#define BMP_280_PRESSURE_ALT_SENSOR
+#ifdef BMP_280_PRESSURE_ALT_SENSOR
+  #define PRESSURE_SENSOR_TIMEOUT_MILLIS 300
+  #define PRESSURE_SENSOR_COMMUNICATION_SPEED
+  #define PRESSURE_SENSOR_UPDATE_HERTZ 40
+#endif
+
+#define UBLOX_GPS_SENSOR
+#ifdef UBLOX_GPS_SENSOR
+  #define GPS_TIMEOUT_MILLIS 2000
+  #define GPS_COMMUNICATION_SPEED 4800
+  #define GPS_UPDATE_HERTZ 1
+#endif
+
+#define MPU9250_IMU_SENSOR
+#ifdef MPU9250_IMU_SENSOR
+  #define IMU_TIMEOUT_MILLIS 10
+  #define IMU_COMMUNICATION_SPEED
+  #define IMU_UPDATE_HERTZ 300
+#endif
 
 //sensor dependent flags
 #define SENSOR_PACKAGE_GPS_NOT_LOCKED -50
@@ -35,7 +61,12 @@ public:
   int enableSensor(int sensorFlag);
 private:
   byte sensorEnabledStatus; //made up of the sensor flags to show enabled, disabled means a 0
-  ulong lastSensorReadMillis[6];
+  UpdateScheduler imuUpdater, pressureUpdater, gpsUpdater;
+  RocketData accelData, gyroData, magData, gpsData, pressureAltData;
+  int initGps();
+  int initIMU();
+  int initPressureSensor();
+  bool newDataAvail[3] = {0,0,0}; //imu, pressureAlt, gps
   #ifdef BMP_280_PRESSURE_ALT
     BMP280 bmp280;
   #endif
@@ -46,5 +77,40 @@ private:
     UBLOX_GPS gps;
   #endif
 };
+
+int SensorPackage::initIMU(){
+  //todo
+  return 0;
+}
+
+int SensorPackage::initPressureSensor(){
+  //todo
+  return 0;
+}
+
+int SensorPackage::initGps(){
+  //todo
+  return 0;
+}
+
+int SensorPackage::init(){
+  //Send error if some sensors fail
+  if(initPressureSensor() != 0) return PRESSURE_SENSOR_INIT_FAILURE;
+  if(initGps() != 0) return GPS_SENSOR_INIT_FAILURE;
+  if(initIMU() != 0) return IMU_SENSOR_INIT_FAILURE;
+  //init all the time based updaters
+  imuUpdater.setUpdateFrequency(IMU_UPDATE_HERTZ);
+  pressureUpdater.setUpdateFrequency(PRESSURE_SENSOR_UPDATE_HERTZ);
+  gpsUpdater.setUpdateFrequency(GPS_UPDATE_HERTZ);
+  return 0;
+}
+
+//return whether new data is available, 1 == true, 0 == false
+//all new data is taged in a global array so to know what data is new and not
+int SensorPackage::update(){
+  //check all the updaters
+
+  return 0;
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <DataConfig.h>
 #include <FLASH_FAT.h>
-#include <BMP280.h>
 #include <SerialBuffer.h>
 #include <CircularBufferIndexer.h>
 
@@ -44,174 +43,6 @@ class FlightController{
 };
 
 //these are important for converting floats and larger values into bytes. Do lose resolution, but so what?
-
-
-//example: SF:0 //send file 0
-//flight time communications
-
-/*
-void sendErrorToComputer(String message){
-  //encode it
-  message = SEND_ERROR+message;
-  handleComputerSend(&message);
-}
-int getFileIndexFromMessage(String message, int *index){
-  int start = message.indexOf(":") + 1;
-  if(start < 0) {
-    sendErrorToComputer("NO FILE INDEX");
-    return -1;
-  }
-  int end = message.length();
-  //if(message.indexOf('\n') >= 0) end = message.indexOf('\n');
-  Serial.println("Parsed index: " + message.substring(start,end));
-  *index = atoi(message.substring(start, end).c_str());
-  return 0;
-}
-
-int handleFileSendRequest(String message){
-  //go parse the message for a file index
-  int index;
-  if(getFileIndexFromMessage(message, &index) != 0) return -1;
-  if(index < 0 || index > fileStructure.numberOfFiles-1){
-    //error!
-    sendErrorToComputer("INVALID FILE INDEX");
-    return -1;
-  }
-  Serial.println("Opening file index: " + String(index));
-  //otherwise, lets start a stream
-  fileSystem.openForRead(index);
-  //go and send
-  status = reportOpenedFileContents();
-}
-
-
-int reportOpenedFileContents(){
-  //go grab space for the maximum message size ~32 bytes
-  byte primary[32];
-  //go and grab the first byte in the file, this is the data descriptor
-  //need some look ahead
-  //byte dataDescriptor = 0;
-  String computerMessage;
-  while(fileSystem.readStream(&primary[0], 1) > 0){
-    switch((int)primary[0]){
-      case (RAW_FLIGHT_DATA_DATA_DESCRIPTOR):
-        //Serial.println("RAW FLIGHT DATA");
-        //grab the remaining length
-        status = fileSystem.readStream(&primary[1],RAW_FLIGHT_DATA_SIZE-1);
-
-        for(int i = 0; i < RAW_FLIGHT_DATA_SIZE; i ++){
-          Serial.print(String(primary[i]) + "\t");
-        }
-
-        status = decodeRawFlightDataFromDataStorage(&primary[0], &buffferedFlightData);
-        computerMessage = encodeRawFlightDataForComputer(&buffferedFlightData);
-        status = handleComputerSend(&computerMessage);
-        if(status == COMMS_FAILURE){
-          sendErrorToComputer("FILE_SEND_SERIAL_FAILURE");
-          return -1;
-        }
-        break;
-      default:
-        //todo
-        Serial.println("Unrecognized!");
-        break;
-      }
-  }
-  return 0;
-}
-
-void updateFileStructure(){
-  fileSystem.readLookupTable(&fileStructure);
-}
-
-void handleFatRequest(String message){
-  //send over the fat table
-  //send each index and length.
-
-  index,size;index,size;index,size
-
-  updateFileStructure();
-  String totalMessage;
-  for(int i = 0; i < fileStructure.numberOfFiles; i ++){
-    totalMessage+=String(i) + ",";
-    totalMessage += String(fileStructure.files[i].size) + ";";
-  }
-  //go and send it
-  status = handleComputerSend(&totalMessage);
-  if(status == COMMS_FAILURE){
-    sendErrorToComputer("FILE_FAT_SERIAL_FAILURE");
-  }
-}
-
-int handleEraseRequest(String message){
-  //get the file index
-  //int index;
-  //if(getFileIndexFromMessage(message, &index) != 0) return -1;
-  //
-  fileSystem.eraseLastFile();
-  fileSystem.readLookupTable(&fileStructure);
-  return 0;
-}
-
-//this is how we communicate with the computer
-int handleComputerCommunication(String message){
-  //main handler
-  //check what the message is, handle appropriately
-  if(message.indexOf(SEND_FILE)>=0){
-    //Serial.println("Sending file!");
-    handleFileSendRequest(message);
-  }
-  else if(message.indexOf(SEND_FAT_TABLE)>=0){
-    handleFatRequest(message);
-
-  }
-  else if(message.indexOf(ERASE_FILE)>=0){
-    handleEraseRequest(message);
-  }
-  else{
-
-  }
-  return -1;
-}
-
-
-
-int initFileSystem(){
-  //first check if we can communicate with the flash
-  status = fileSystem.init(PA4,true,false);
-  if(status != 0){
-    Serial.println("Init Filesystem error! " + String(status));
-    while(true);
-  }
-  else{
-    Serial.println("Init Filesystem success! ");
-  }
-  //then make sure we have a filesystem
-  status = fileSystem.readLookupTable(&fileStructure);
-  //go check the number of files and make sure we're not full
-  if((fileStructure.numberOfFiles > 250) | ((fileStructure.files[fileStructure.numberOfFiles-1].startAddress + fileStructure.files[fileStructure.numberOfFiles-1].size) >= 8000000L)){
-    Serial.println("Running out of space!");
-  }
-  //otherwise, we're good!
-  return 0;
-}
-
-
-  recordFlight
-  main function for flight routines
-  records all available data and controls data storage
-  six distinct flight phases: waitingForLaunch, Boost, Coast, Apogee, Recovery, Landed
-  serial interupt pauses all flight systems as this is a sign we are not actually in flight, or that recovery is happening
-
-  the waitForLaunch uses a circular buffer of ~5 seconds
-*/
-
-//Raw_Flight_Data flightDataPreLaunchBuffer[PRE_LAUNCH_RAW_FLIGHT_BUFFER_SIZE];
-//GPS_data gpsDataPreLaunchBuffer[PRE_LAUNCH_GPS_BUFFER_SIZE];
-
-//CircularBufferIndexer flightDataPreLaunchBufferIndexer;
-//CircularBufferIndexer gpsDataPreLaunchBufferIndexer;
-
 
 bool serialInterrupt = false;
 
@@ -261,51 +92,9 @@ void checkPhaseChange(){
       break;
   }
 }
-/*
-void recordFlight(){
-  //main idea is to check and handle user commands
-  if(computerBuffer.available() > 0){
-    if(!serialInterrupt){
-      serialInterrupt = true;
-      handleStandDown();
-    }
-    String message;
-    computerBuffer.readLine(&message);
-    handleComputerCommunication(message);
-  }
-  if(!serialInterrupt){
-    //standard data collection
-    checkPhaseChange();
-  }
 
-}
-*/
-/*
-void makeDummyFile(){
-  fileSystem.openToWrite();
-  byte buffer[256];
-  for(int i = 0; i < 256; i ++){
-    buffer[i] = i;
-  }
-  fileSystem.write(&buffer[0], 256);
-  fileSystem.close();
-}
 
-void makeDummyFlightFile(){
-  Raw_Flight_Data data;
-  fileSystem.openToWrite();
-  byte tempHold[RAW_FLIGHT_DATA_SIZE];
-  for(int i = 0; i < 256; i ++){
-    data.timeStamp = i*10;
-    data.acceleration = i/100.0;
-    data.pressureAlt = i*2;
-    encodeRawFlightDataForDataStorage(&tempHold[0], &data);
-    fileSystem.write(&tempHold[0],RAW_FLIGHT_DATA_SIZE);
-  }
-  fileSystem.close();
-}
 
-*/
 void setup() {
   Serial.begin(115200);
   delay(2000);
@@ -318,7 +107,7 @@ void setup() {
   //coms.init();
   String msg;
   //make up a couple of sensor packages
-  RocketData dummyAccel, dummyGyro, dummyMag;
+  RocketData dummyAccel, dummyGyro;
   dummyAccel.tag = FLIGHT_ACCEL_DATA_TAG;
   dummyAccel.timeStamp = 100;
   dummyAccel.data = new float[3]{10,20,40};
@@ -354,7 +143,7 @@ void setup() {
   //initFileSystem();
   //makeDummyFlightFile();
   //fileSystem.makeFileAllocationTable();
-  /*
+
   while(true){
     while(computerBuffer.available() <=0){
       delay(1);
